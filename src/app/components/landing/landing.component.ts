34c4fd848507d2caaa8e83cdb582312cd5fbe7f6
Eston Kagwima <ekagwima745@gmail.com>
2021-06-21 21:53:56 +0300

    fixing syntax error angular

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Emitters } from 'src/app/emitters/emitters';
-
+​
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
+​
   user:any = []
   message =" "
-
+​
   name='csrftoken'
   constructor(private http:HttpClient) { }
-
+​
   ngOnInit(): void {
           this.http.get('http://localhost:8000/api/user/current-user/',
               {withCredentials:true})
@@ -23,18 +23,16 @@ export class LandingComponent implements OnInit {
                   
                   console.log(response);
                   this.user = response;
-
+​
                   Emitters.authEmitter.emit(true)
               },
               error => {
                 console.log('error', error)
                 this.message ="no user found";
-
+​
             }
         );
         
   }
-);
-}
-
+​
 }
