38fa5260adb43390a2b11f30c9529f35fd35425d
Eston Kagwima <ekagwima745@gmail.com>
2021-06-21 17:03:49 +0300

    changing ts file for landing

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  user:any = []
+  message =" "
+  constructor(  private http: HttpClient,) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:8000/api/user/current-user/',
+    {withCredentials:true})
+    .subscribe(response =>{
+
+        console.log(response);
+        this.user = response;
+        console.log(this.user.first_name)
+    },
+    error => {
+      console.log('error', error)
+      this.message ="no user found";
+      //redirect
   }
+);
+}
 
 }
